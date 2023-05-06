e74662798a3e62268943a613da8c18c2109a934d
Mutayyabweb <126829771+Mutayyabweb@users.noreply.github.com>
2023-05-06 12:24:08 +0100

    Numbers

---
+++ script.js
@@ -0,0 +1,87 @@
+// //  Variables, Constants and Comments
+
+// var score=34;
+// var score=56;//redeclare
+// console.log(score);
+
+// let points=34;
+// points=55;
+// console.log(points);
+
+// // constants
+// const value =22;
+// console.log(value);
+
+
+// Data Types of Values
+
+// 7 Types of Data
+
+// Number 1,2,3,4,5,6,7,8,9
+// Strings "22","mutayyab","hussain" 'hello' `hello ${Kase ho}`
+// Boolean true/false
+// null is a special data type. null is a intentionally absence of value
+// let scroe =null;
+// scroe=55
+// console.log(scroe);
+
+// Undefined Variable declared but not intialise
+// let points;
+// console.log(points);
+
+// object complex data structure
+// Symbol looks like object
+
+
+// Strings
+// let firstName="wow";
+// let lastName="Programmer";
+// console.log(firstName+" "+lastName);    
+
+//  method 2 template literals
+// let firstName="wow";
+// let lastName="programmer";
+// let fullName=`I Want to Become a ${firstName} ${lastName}?`
+// console.log(fullName);
+
+
+
+// Strings Methods
+// let firstName="wow"
+// let lastName="Programmer"
+// getting string characters
+// console.log(firstName[1]);
+// console.log(firstName.toLowerCase());
+// console.log(firstName.toUpperCase());
+// console.log(lastName.indexOf("e"));
+// console.log(lastName.lastIndexOf("Programmer"));
+
+// Common Strings Methods
+// let hobbies='coding reading running';
+// console.log(hobbies);
+// trim Method
+// console.log(hobbies.trim());
+// Includes method
+// console.log(hobbies.includes("ho")); //it returns boolean
+// Slice Method
+// let fullName="wowProgrammer"
+// console.log(fullName.slice(0,5)); //last index exculded (0,5)it give value(0,4) 
+
+
+// string split method
+// let favoritColor="Brown Blue Black Grey"
+// let arrColor=favoritColor.split('');
+// console.log(arrColor);
+
+// javaScript Strings are Imutable
+// let str="Hello";
+// str[0]="p"
+// console.log(str);   
+
+
+// Numbers
+let score =50;
+// console.log(score,typeof(score));
+
+// Mathmaticall Operators * / % ** + -
+let result =score/2; //Give Quotient  
